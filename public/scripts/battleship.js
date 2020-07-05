048100ab1e09a7b0d4b62e112c95fb83ae921fe9
William <mack.mindenhall@gmail.com>
2020-07-05 14:25:58 +0000

    Fixed a bug in the ship placement logic

--- public/scripts/battleship.js
+++ public/scripts/battleship.js
@@ -2,79 +2,102 @@ import { createTotalGridDiv, createBoard } from "./components/makeBoard.js";
 import { createShipDiv, createShips } from "./components/ships.js";
 import { PLAYER1SHIPS } from "./components/constants.js";
 
-// What i basically need is:
-// if ($(`#${el.target.id}`).attr("filled") === "false" && clicks > 1) {
-//   let firstRow = Object.keys(PLAYER1SHIPS.carrier.position)[0]
-//     .split("-")[0]
-//     .charCodeAt(0);
-//   let firstCol = Number(
-//     Object.keys(PLAYER1SHIPS.carrier.position)[0].split("-")[1]
-//   );
+// let clicks = 0;
+// $(document).on("click", ".carrier-wrapper", (e) => {
+//   $(document).one("click", ".grid-item", (el) => {
+//     if ($(`#${el.target.id}`).attr("filled") === "false") {
+//       if (clicks > 0) {
+//         let prevRow = Object.keys(PLAYER1SHIPS.carrier.position)
+//           [clicks - 1].split("-")[0]
+//           .charCodeAt(0);
+//         let prevCol = Number(
+//           Object.keys(PLAYER1SHIPS.carrier.position)[clicks - 1].split("-")[1]
+//         );
 
-//   let currentRow = el.target.id.split("-")[0].charCodeAt(0);
-//   let currentCol = Number(el.target.id.split("-")[1]);
+//         let currentRow = el.target.id.split("-")[0].charCodeAt(0);
+//         let currentCol = Number(el.target.id.split("-")[1]);
 
-//   let rowDif = Math.abs(firstRow - currentRow);
-//   let colDif = Math.abs(firstCol - currentCol);
+//         let rowDif = Math.abs(prevRow - currentRow);
+//         let colDif = Math.abs(prevCol - currentCol);
 
-//   if(rowDif < 5 && colDif === 0) {
+//         if ((rowDif < 5 && colDif === 0) || (colDif < 5 && rowDif === 0)) {
+//           $(`#${el.target.id}`)
+//             .attr("filled", "true")
+//             .css({ backgroundColor: "orange" });
+//           PLAYER1SHIPS.carrier.position[el.target.id] = false;
+//           console.log("PLAYER1SHIPS: ", PLAYER1SHIPS.carrier);
 
-//   }
-// }
+//           $(`#${e.target.id}`).css({ display: "none" });
+//           clicks++;
+//         }
+//       } else {
+//         $(`#${el.target.id}`)
+//           .attr("filled", "true")
+//           .css({ backgroundColor: "orange" });
+//         PLAYER1SHIPS.carrier.position[el.target.id] = false;
 
-// Two possibilities for checking that ships are placed in a line
-//  First (and simpler but uglier idea i think) way is to just simply check if the adjacent square is filled by the same ship elem
-//  Second way would be to have maybe a while loop that checks while clicks is less than ship length, check
+//         $(`#${e.target.id}`).css({ display: "none" });
+//         clicks++;
+//       }
+//     }
+//   });
+// });
 
-let clicks = 0;
-$(document).on("click", ".carrier-wrapper", (e) => {
+$(document).on("click", ".ship", (e) => {
+  let currentShip = e.target.className.split("-")[0];
+  let clicks = Object.keys(PLAYER1SHIPS[currentShip].position).length;
   $(document).one("click", ".grid-item", (el) => {
     if ($(`#${el.target.id}`).attr("filled") === "false") {
       if (clicks > 0) {
-        console.log(
-          "If statement checking that clicks is greater than 1 executes"
+        let firstRow = Object.keys(PLAYER1SHIPS[currentShip].position)[0]
+          .split("-")[0]
+          .charCodeAt(0);
+        let firstCol = Number(
+          Object.keys(PLAYER1SHIPS[currentShip].position)[0].split("-")[1]
         );
-        let prevRow = Object.keys(PLAYER1SHIPS.carrier.position)
+        let prevRow = Object.keys(PLAYER1SHIPS[currentShip].position)
           [clicks - 1].split("-")[0]
           .charCodeAt(0);
         let prevCol = Number(
-          Object.keys(PLAYER1SHIPS.carrier.position)[clicks - 1].split("-")[1]
+          Object.keys(PLAYER1SHIPS[currentShip].position)[clicks - 1].split(
+            "-"
+          )[1]
         );
 
-        // Find the difference of the first placed element to the
         let currentRow = el.target.id.split("-")[0].charCodeAt(0);
         let currentCol = Number(el.target.id.split("-")[1]);
 
         let rowDif = Math.abs(prevRow - currentRow);
         let colDif = Math.abs(prevCol - currentCol);
 
-        console.log("rowDif: ", rowDif);
-        console.log("colDif: ", colDif);
+        let totalRowDif = Math.abs(firstRow - currentRow);
+        let totalColDif = Math.abs(firstCol - currentCol);
 
-        if ((rowDif < 5 && colDif === 0) || (colDif < 5 && rowDif === 0)) {
-          console.log("if statement checking row and column diff works works");
+        if (
+          (rowDif < PLAYER1SHIPS[currentShip].size &&
+            colDif === 0 &&
+            totalRowDif < PLAYER1SHIPS[currentShip].size) ||
+          (colDif < PLAYER1SHIPS[currentShip].size &&
+            rowDif === 0 &&
+            totalColDif < PLAYER1SHIPS[currentShip].size)
+        ) {
           $(`#${el.target.id}`)
             .attr("filled", "true")
             .css({ backgroundColor: "orange" });
-          PLAYER1SHIPS.carrier.position[el.target.id] = false;
-          console.log("PLAYER1SHIPS: ", PLAYER1SHIPS.carrier);
+          PLAYER1SHIPS[currentShip].position[el.target.id] = false;
 
           $(`#${e.target.id}`).css({ display: "none" });
           clicks++;
         }
-      }
-      //
-      else {
+      } else {
         $(`#${el.target.id}`)
           .attr("filled", "true")
           .css({ backgroundColor: "orange" });
-        PLAYER1SHIPS.carrier.position[el.target.id] = false;
-        console.log("PLAYER1SHIPS: ", PLAYER1SHIPS.carrier);
+        PLAYER1SHIPS[currentShip].position[el.target.id] = false;
 
         $(`#${e.target.id}`).css({ display: "none" });
         clicks++;
       }
-      console.log("clicks: ", clicks);
     }
   });
 });
